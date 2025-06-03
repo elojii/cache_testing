8f1b5ab469cfecf5c51c627ad6a9481eaf9dac3a
andrii <mantula.andrey@gmail.com>
2025-06-03 13:07:36 +0300

    zxhopa

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
